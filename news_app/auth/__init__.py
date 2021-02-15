6639b350a1546107aa7a9ec6a0bc3721a336aadf
raghav1674 <raghav.81-cse-17@mietjammu.in>
2021-02-15 15:03:37 +0530

    modal form for register created

--- news_app/auth/__init__.py
+++ news_app/auth/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint 
 
 # auth blueprint for our news_app
-auth = Blueprint("auth",__name__,template_folder='templates',static_folder='static')
+auth = Blueprint("auth",__name__,template_folder='templates',static_folder='static',static_url_path='')
 
 
 from . import routes
